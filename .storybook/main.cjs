729edc5d3850921702a9d192db98ad6e6763f4c8
Juliana <julianamq20@gmail.com>
2022-10-14 07:36:33 -0300

    2

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -18,7 +18,7 @@ module.exports = {
   },
   "viteFinal": (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignitelab-juliana/'
+      config.base = '/igniteLabJuliana/'
     }
 
     return config;
